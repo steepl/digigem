b08864093404f33371e3d37d133587e8a448e1bd
steepl <50868613+steepl@users.noreply.github.com>
2022-09-18 15:17:09 +0200

    disabled all resource files except javascript.json

--- resources/index.js
+++ resources/index.js
@@ -1,21 +1,21 @@
-import css from './css'
-import daily from './daily'
-import design from './design'
-import frontend from './frontend'
-import fullstack from './fullstack'
-import general from './general'
-import html from './html'
+// import css from './css'
+// import daily from './daily'
+// import design from './design'
+// import frontend from './frontend'
+// import fullstack from './fullstack'
+// import general from './general'
+// import html from './html'
 import javascript from './javascript'
-import php from './php'
-import podcasts from './podcasts'
-import python from './python'
-import ruby from './ruby'
-import server from './server'
-import utility from './utility'
-import icons from './icons'
-import accessibility from './accessibility'
-import rust from './rust'
-import contentCreators from './content-creators'
+// import php from './php'
+// import podcasts from './podcasts'
+// import python from './python'
+// import ruby from './ruby'
+// import server from './server'
+// import utility from './utility'
+// import icons from './icons'
+// import accessibility from './accessibility'
+// import rust from './rust'
+// import contentCreators from './content-creators'
 
 const sortByTitle = ({ title: titleA }, { title: titleB }) => {
   if(titleA < titleB) return -1
@@ -24,22 +24,22 @@ const sortByTitle = ({ title: titleA }, { title: titleB }) => {
 }
 
 export default [
-  css,
-  daily,
-  design,
-  frontend,
-  fullstack,
-  general,
-  html,
+  // css,
+  // daily,
+  // design,
+  // frontend,
+  // fullstack,
+  // general,
+  // html,
   javascript,
-  php,
-  podcasts,
-  python,
-  ruby,
-  server,
-  utility,
-  icons,
-  accessibility,
-  rust,
-  contentCreators,
+  // php,
+  // podcasts,
+  // python,
+  // ruby,
+  // server,
+  // utility,
+  // icons,
+  // accessibility,
+  // rust,
+  // contentCreators,
 ].sort(sortByTitle)
