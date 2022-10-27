058dcbd833d0337d0dcea7fb3c29a6c7b40f3a16
steepl <50868613+steepl@users.noreply.github.com>
2022-10-27 14:17:25 +0200

    corrected filter button icons, updated resources and glossary page

--- pages/glossary.vue
+++ pages/glossary.vue
@@ -2,12 +2,12 @@
   div
     h1 Glossary
     p Op deze pagina vind je uitleg over de verschillende termen die gebruikt worden op Digigem.
+    h2 HTML
+    p HTML staat voor HyperText Markup Language, en is de standaard opmaaktaal voor websites. Waar CSS vooral bedoelt is voor styling, en JavaScript voor het stukje logica, wordt HTML gebruikt om het geraamte (skelet) van de pagina te bouwen. Om dit te kunnen doen maakt het gebruik van tags en elementen.
     h2 JavaScript
     p JavaScript is een programmeertaal die veel gebruikt wordt voor front-end web development. Het vormt samen met HTML en CSS de gouden driehoek voor het maken van interactieve websites en webapplicaties. Naast front-end, kan JavaScript ook gebruikt worden aan de achterkant (back-end) van een applicatie. Eén van de bekendste tools hiervoor is Node.js.
     h2 Python
-    p Python is een general purpose programmeertaal ontworpen met het oog op simpliciteit en (code) leesbaarheid. Het is een populaire keuze voor beginners en mensen die snel een taak willen automatiseren. Python is daarnaast ook erg populair onder data analisten, waar tools zoals Jupyter Notebook en Pandas het gemakkelijk maken om grote hoeveelheden data om te zetten in grafieken of tabellen.
-    h2 HTML
-    p HTML staat voor HyperText Markup Language en is de standaard opmaaktaal voor websites. Waar CSS vooral bedoelt is voor styling, en JavaScript voor het stukje logica, wordt HTML gebruikt om het geraamte (skelet) van een pagina weer te geven. Om dit te doen maakt HTML gebruik van tags en elementen.
+    p Python is een general purpose programmeertaal ontworpen met het oog op simpliciteit en (code) leesbaarheid. Het is een populaire keuze voor beginners en mensen die snel een taak willen automatiseren. Python is daarnaast ook erg populair onder data analisten, waar tools zoals Jupyter Notebook en Pandas met gemak grote hoeveelheden data om kunnen zetten in grafieken of tabellen.
 </template>
   
 <style lang="scss" scoped>
