9a8557b0dc344078a59ad6928b9bbb3c1ec6e30c
walkxcode <walkxnl@gmail.com>
2023-04-04 00:29:36 +0200

    💄 Changes font to Work Sans

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
 			{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
 			{
 				rel: "stylesheet",
-				href: "https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap",
+				href: "https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600;700;800&display=swap",
 			},
 		],
 	},
